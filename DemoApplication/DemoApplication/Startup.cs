01addddc9940f4077da0b64ca650bfb2f48c8d14
rohitkat10 <rohitkat10@gmail.com>
2017-03-06 07:43:20 +0530

    6March project Demo Asp.net MVC 5

---
+++ DemoApplication/DemoApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoApplication.Startup))]
+namespace DemoApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
